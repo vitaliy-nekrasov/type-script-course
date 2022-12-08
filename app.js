cb4230f3a0acde76b9dc1c10aa77ac914be3e7ef
Vitaliy7171 <vitaliynekrasov1@gmail.com>
2022-12-08 10:04:35 +0200

    Done 8 and 9 task

--- app.js
+++ app.js
@@ -22,23 +22,54 @@ if (typeof some === "string") {
 // let person = ['Max', 21];
 var person;
 person = ["Max", 21];
-// Опишите enum условие следующее, он должен отображать статус загрузки. Загружается (LOADING) и загружена (READY).
-var Loading;
-(function (Loading) {
-    Loading[Loading["LOADING"] = 0] = "LOADING";
-    Loading[Loading["READY"] = 1] = "READY";
-})(Loading || (Loading = {}));
+// Опишите enum условие следующее, он должен отображать статус загрузки.
+// Загружается(LOADING) и загружена(READY).
+var IsLoading;
+(function (IsLoading) {
+    IsLoading[IsLoading["LOADING"] = 0] = "LOADING";
+    IsLoading[IsLoading["READY"] = 1] = "READY";
+})(IsLoading || (IsLoading = {}));
 var image = {
-    isLoading: Loading.READY
+    isLoading: IsLoading.READY
 };
-if (image.isLoading === Loading.READY) {
-    console.log("IsLoading: ", Loading.READY);
+if (image.isLoading === IsLoading.READY) {
+    console.log("IsLoading: ", IsLoading.READY, "Изображение загрузилось");
+}
+if (image.isLoading === IsLoading.LOADING) {
+    console.log("IsLoading: ", IsLoading.LOADING, "Изображение загружается");
 }
 // Сделайте переменную, которая может принимать или строку или число.
 var personAge;
 personAge = 10;
 personAge = "10";
-// Сделайте переменную, которая может принимать только одно значение из двух 'enable' или 'disable'
+// Сделайте переменную, которая может принимать только одно значение из двух 'enable'
+// или 'disable'
 var darkThemeIsEnable;
 darkThemeIsEnable = "enable";
 darkThemeIsEnable = "disable";
+// Укажите типы для следующих функций
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
+    return num1 + num2;
+}
+function customError() {
+    throw new Error("Error");
+}
+var page1 = {
+    title: "The awesome page",
+    likes: 100,
+    accounts: ["Max", "Anton", "Nikita"],
+    status: "open",
+    details: {
+        createAt: "2021-01-01",
+        updateAt: "2021-05-01"
+    }
+};
+var page2 = {
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "close"
+};
--- app.ts
+++ app.ts
@@ -26,7 +26,8 @@ if (typeof some === "string") {
 let person: [string, number];
 person = ["Max", 21];
 
-// Опишите enum условие следующее, он должен отображать статус загрузки. Загружается (LOADING) и загружена (READY).
+// Опишите enum условие следующее, он должен отображать статус загрузки.
+// Загружается(LOADING) и загружена(READY).
 enum IsLoading {
   LOADING,
   READY,
@@ -35,7 +36,10 @@ const image = {
   isLoading: IsLoading.READY,
 };
 if (image.isLoading === IsLoading.READY) {
-  console.log("IsLoading: ", IsLoading.READY);
+  console.log("IsLoading: ", IsLoading.READY, "Изображение загрузилось");
+}
+if (image.isLoading === IsLoading.LOADING) {
+  console.log("IsLoading: ", IsLoading.LOADING, "Изображение загружается");
 }
 
 // Сделайте переменную, которая может принимать или строку или число.
@@ -43,7 +47,51 @@ let personAge: string | number;
 personAge = 10;
 personAge = "10";
 
-// Сделайте переменную, которая может принимать только одно значение из двух 'enable' или 'disable'
+// Сделайте переменную, которая может принимать только одно значение из двух 'enable'
+// или 'disable'
 let darkThemeIsEnable: "enable" | "disable";
 darkThemeIsEnable = "enable";
 darkThemeIsEnable = "disable";
+
+// Укажите типы для следующих функций
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error("Error");
+}
+
+// Создайте свой тип данных на основе имеющихся данных.
+type DataType = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: string;
+    updateAt: string;
+  };
+};
+
+const page1: DataType = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: "2021-01-01",
+    updateAt: "2021-05-01",
+  },
+};
+
+const page2: DataType = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
